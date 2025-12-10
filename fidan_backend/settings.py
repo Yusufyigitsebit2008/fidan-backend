6ed3202fbc940d6fc1919d133a5ddc7d89f14058
Yusuf yiğit Şebit <yusufyigitsebit@gmail.com>
2025-12-10 16:13:23 +0300

    JWT ve Auth sistemleri eklendi

--- fidan_backend/settings.py
+++ fidan_backend/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -124,3 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
--- fidan_backend/urls.py
+++ fidan_backend/urls.py
@@ -3,6 +3,11 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from core.views import SchoolViewSet, InventoryViewSet, RequestViewSet
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 router = DefaultRouter()
 router.register(r'schools', SchoolViewSet)       
 router.register(r'inventory', InventoryViewSet)  
@@ -11,4 +16,6 @@ router.register(r'requests', RequestViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)), 
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
